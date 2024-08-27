c27e82fc64220e89c008903ab3e3c1a1fba94e77
Timo Stolz <timo.stolz@gmail.com>
2024-08-27 12:15:22 +0200

    re-configure eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,12 +9,6 @@ export default withNuxt(
     },
   },
   {
-    files: ["app/pages/**.vue"],
-    rules: {
-      "vue/multi-word-component-names": "off",
-    },
-  },
-  {
     rules: {
       "vue/max-attributes-per-line": [
         "error",
