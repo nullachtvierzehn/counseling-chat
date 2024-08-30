318ca5163214b45640da57d31d41a5d60d05fdf1
Timo Stolz <timo.stolz@gmail.com>
2024-08-30 11:40:12 +0200

    Simplify reset of current migrations

--- migrations/current/0001-reset.sql
+++ migrations/current/0001-reset.sql
@@ -1,14 +1,8 @@
-drop policy if exists see_as_dispatcher on app_public.consultations;
-
 drop table if exists app_hidden.consultation_participants_history;
-
 drop table if exists app_public.consultation_participants;
-
-drop type if exists app_public.consultation_role;
+drop table if exists app_public.consultations;
 
 drop function if exists app_public.current_user_consultation_ids();
-
-drop table if exists app_public.consultations;
 
 alter table app_public.organization_memberships
   drop column if exists is_dispatcher,
