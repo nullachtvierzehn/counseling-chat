9597dc944f7591ce6ed3e6eb3e346f6a8a121109
Timo Stolz <timo.stolz@gmail.com>
2024-10-18 22:32:24 +0200

    Add settings for graphql-eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,52 @@
 // @ts-check
+import * as graphql from "@graphql-eslint/eslint-plugin"
+
 import withNuxt from "./.nuxt/eslint.config.mjs"
+
+// Stolen from
+// https://github.com/dimaMachina/graphql-eslint/blob/c9cbf6d8065740302cfb75b278733701dd5f7cf6/examples/vue-code-file/eslint.config.js
+const graphqlChecks = {
+  files: ["**/*.graphql"],
+  languageOptions: {
+    parser: graphql.parser,
+    parserOptions: {
+      graphQLConfig: {
+        schema: "schema.graphql",
+        documents: ["./graphql/**/*.graphql"],
+      },
+    },
+  },
+  plugins: {
+    "@graphql-eslint": {
+      rules: graphql.rules
+    },
+  },
+  rules: {
+    ...graphql.configs["flat/operations-recommended"].rules,
+    "@graphql-eslint/no-anonymous-operations": "error",
+    "@graphql-eslint/no-duplicate-fields": "error",
+    "@graphql-eslint/naming-convention": [
+      "error",
+      {
+        OperationDefinition: {
+          style: "PascalCase",
+          forbiddenPrefixes: ["Query", "Mutation", "Subscription", "Get"],
+          forbiddenSuffixes: ["Query", "Mutation", "Subscription"],
+        },
+      },
+    ],
+  },
+}
+
+const graphqlInVue = {
+  files: ["**/*.vue"],
+  processor: graphql.processors.graphql,
+}
 
 export default withNuxt(
   {
-    files: ["**/*.ts", "**/*.tsx"],
     rules: {
-      "no-console": "warn", // allow console.log in TypeScript files
-    },
-  },
-  {
-    rules: {
+      "no-console": "warn",
       "vue/max-attributes-per-line": [
         "error",
         {
@@ -20,4 +57,6 @@ export default withNuxt(
       "@typescript-eslint/no-explicit-any": "warn",
     },
   },
+  // graphqlInVue,
+  graphqlChecks
 )
