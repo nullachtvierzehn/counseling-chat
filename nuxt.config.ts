330c6155078a770735a8870d76a858146e861e7b
Timo Stolz <timo.stolz@nullachtvierzehn.de>
2024-11-07 15:50:09 +0100

    Install `@pinia/nuxt`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/eslint", "@vueuse/nuxt", "@nuxt/ui"],
+  modules: ["@nuxt/eslint", "@vueuse/nuxt", "@nuxt/ui", "@pinia/nuxt"],
   devtools: { enabled: true },
   css: [
     "prosemirror-view/style/prosemirror.css",
