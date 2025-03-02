bb6cc30e3d1cde66a1354e88a0f2d71d9c4cc1d8
Timo Stolz <timo.stolz@gmail.com>
2025-03-02 08:09:06 +0100

    Use new built-in websocket handler

--- server/api/graphql.ts
+++ server/api/graphql.ts
@@ -1,6 +1,6 @@
-import { CloseCode } from "graphql-ws"
-import { server, graphqlWsServer } from "../graphile/server"
+import { server } from "../graphile/server"
 
+// Adapted from https://grafast.org/grafserv/servers/nuxt/#graphql-endpoint
 export default eventHandler({
   /**
    * HTTP request handler
@@ -13,45 +13,5 @@ export default eventHandler({
   /**
    * WS request handler
    */
-  websocket: {
-    open(peer) {
-      const { ws: socket, req } = peer.ctx.node
-      const closed = graphqlWsServer.opened(
-        {
-          protocol: socket.protocol, // will be validated
-          send: data =>
-            new Promise((resolve, reject) => {
-              socket.send(data, (err: Error) =>
-                err ? reject(err) : resolve(),
-              )
-            }), // control your data flow by timing the promise resolve
-          close: (code, reason) => socket.close(code, reason), // there are protocol standard closures
-          onMessage: cb =>
-            socket.on("message", async (event: any) => {
-              try {
-                // wait for the the operation to complete
-                // - if init message, waits for connect
-                // - if query/mutation, waits for result
-                // - if subscription, waits for complete
-                await cb(event.toString())
-              }
-              catch (err: any) {
-                try {
-                  // all errors that could be thrown during the
-                  // execution of operations will be caught here
-                  socket.close(CloseCode.InternalServerError, err.message)
-                }
-                catch {
-                  /* noop */
-                  console.error("failed to close socket")
-                }
-              }
-            }),
-        },
-        // pass values to the `extra` field in the context
-        { socket, request: req },
-      )
-      socket.once("close", closed)
-    },
-  },
+  websocket: server.makeWsHandler()
 })
